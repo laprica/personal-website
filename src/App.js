29ee798c19de0b18a4c1cb234f592d869fd7054a
Leanna Pancoast - Gwista <laprica@users.noreply.github.com>
2020-01-10 19:12:05 -0500

    use w3css and display initial face and buttons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 class="w3-center">Leanna Pancoast</h1>
+      <div class="w3-display-container w3-center" style={{width:"50%", transform:"translateX(50%)"}}>
+        <img src={require("./assets/profilePic.jpg")} alt="picture of Leanna Pancoast" class="w3-image w3-circle"></img>
+        <div class="w3-display-bottomleft w3-container">
+            <button class="w3-button w3-yellow">About</button></div>
+        <div class="w3-display-bottomright w3-container">
+            <button class="w3-button w3-yellow">Contact</button>
+        </div>
+      </div>
     </div>
   );
 }
