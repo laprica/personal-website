220a533716c69d0ede45909ae3692719968c6193
Leanna Pancoast - Gwista <laprica@users.noreply.github.com>
2020-01-10 23:59:33 -0500

    name back to top of circle, about/contact buttons respond to size

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {ProjectListDisplay} from './ProjectListDisplay.js';
 function App() {
   return (
     <div className="App">
+      <h1 className="mainTitle">Leanna Pancoast</h1>
       <div className="w3-display-container w3-center" style={{width:"50%", transform:"translateX(50%)"}}>
         <img src={"/assets/profilePic.jpg"} alt="Leanna Pancoast" className="w3-image w3-circle"></img>
         {/* <div className="w3-display-bottomleft w3-container">
@@ -14,13 +15,11 @@ function App() {
             <button className="w3-button w3-yellow">Contact</button>
         </div> */}
       </div>
-      <div className="w3-container nav-bar w3-display-container">
-          
-          <div className="w3-container nav-item nav-item-about">
+      <div className="w3-container nav-bar">
+          <div className="w3-container nav-item" id="about">
               <button className="w3-button w3-yellow ">About</button>
           </div>        
-          <div className="w3-center nav-item nav-item-name">Leanna Pancoast</div>
-          <div className="w3-container nav-item nav-item-contact">
+          <div className="w3-container nav-item" id="contact">
               <button className="w3-button w3-yellow">Contact</button>
           </div>
       </div>
